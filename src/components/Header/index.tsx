1982748deba0f2dde2e177d615377856e34ef742
Geraldo <geraldo@easymine.com.br>
2022-01-11 20:53:52 -0300

    SignIn

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
-import styles from './syteles.module.scss'
+import { SignInButton } from '../SignInButton'
+import styles from './styles.module.scss'
 
 export function Header() {
   return (
@@ -9,6 +10,8 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton/>
       </div>
     </header>
   )
