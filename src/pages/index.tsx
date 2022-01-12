d95009ce2da828155682167bd135098761e47dcc
Geraldo <geraldo@easymine.com.br>
2022-01-12 12:57:59 -0300

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
 
     <>
       <Head>     
-        <title>Início | IG.News</title>
+        <title>Home | IG.News</title>
       </Head>
-        <h1>
-        Hello World 
-        </h1>
+
+     <main>
+       <section>
+        <span> 👏 Hey, Welcome</span>
+        <h1>News About the <span>React</span> World</h1>
+        <p>
+          Get acess to all the publications <br />
+          <span>for $9,90</span>
+        </p>
+         
+       </section>
+       <Image src="/images/avatar.svg" alt="Girl Coding" width={500} height={500}/>
+     </main>
     </>
   )
 }
