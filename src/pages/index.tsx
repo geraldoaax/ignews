0e6eb4e9c94baa2f9b7509a6301e803cde99246d
Geraldo <geraldo@easymine.com.br>
2022-01-12 18:35:32 -0300

    SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
 
 export default function Home() {
   return (
 
     <>
-      <Head>     
+      <Head>
         <title>Home | IG.News</title>
       </Head>
 
-     <main  className={styles.contentContainer}>
-       <section className={styles.hero}>
-        <span> 👏 Hey, Welcome</span>
-        <h1>News About the <span>React</span> World.</h1>
-        <p>
-          Get acess to all the publications <br />
-          <span>for $9,90 month</span>
-        </p>
-         
-    
-       </section>
-       <Image  src="/images/avatar.svg" alt="Girl Coding" width={500} height={500}/>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, Welcome</span>
+          <h1>News About the <span>React</span> World.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9,90 month</span>
+          </p>
+          <SubscribeButton />
 
-     </main>
+
+        </section>
+        <Image src="/images/avatar.svg" alt="Girl Coding" width={500} height={500} />
+
+      </main>
     </>
   )
 }
