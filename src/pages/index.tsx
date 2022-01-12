fdd12819a4e8dd36f37cbefad68a47057aa42da0
Geraldo <geraldo@easymine.com.br>
2022-01-12 18:12:20 -0300

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -9,17 +10,19 @@ export default function Home() {
         <title>Home | IG.News</title>
       </Head>
 
-     <main>
-       <section>
+     <main  className={styles.contentContainer}>
+       <section className={styles.hero}>
         <span> 👏 Hey, Welcome</span>
-        <h1>News About the <span>React</span> World</h1>
+        <h1>News About the <span>React</span> World.</h1>
         <p>
           Get acess to all the publications <br />
-          <span>for $9,90</span>
+          <span>for $9,90 month</span>
         </p>
          
+    
        </section>
-       <Image src="/images/avatar.svg" alt="Girl Coding" width={500} height={500}/>
+       <Image  src="/images/avatar.svg" alt="Girl Coding" width={500} height={500}/>
+
      </main>
     </>
   )
