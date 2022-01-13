6b32e828ae7e070b545e8ecb5862d45a3c0a81c1
Geraldo <geraldo@easymine.com.br>
2022-01-13 18:23:44 -0300

    Auth GitHub Ok

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,33 @@
 import { FaGithub } from 'react-icons/fa'
 import styles from './styles.module.scss'
 import { FiX } from 'react-icons/fi'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 
-export function SignInButton ()  {
-const isUserLoggedIn = true; 
+export function SignInButton() {
+  const { data: session, status } = useSession()
 
-  return isUserLoggedIn ? (
-      <button 
-        type="button"
-        className={styles.signInButton}>
-       <FaGithub color="#04d361"/>
-      Geraldo Dutra Neto
-      <FiX color="#737380" className={styles.closeIcon}/>
-   </button>    
+  console.log(session)
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
 
   ) : (
-        <button 
-          type="button"
-          className={styles.signInButton}>
-        <FaGithub color="#eba417"/>
-        Sign In With GitHub
-        </button>    
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color="#eba417" />
+      Sign In With GitHub
+    </button>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import App, { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 import '../styles/global.scss'
+import { SessionProvider } from "next-auth/react"
 
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return  (
-    <>
-    <Header />
-     <Component {...pageProps} />
-     </>
-     )
+  return (
+    <SessionProvider session={session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
