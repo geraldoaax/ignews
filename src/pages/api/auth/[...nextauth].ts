32e89b9729e9aaff1bf80f5769cedd5475f483a0
Geraldo <geraldo@easymine.com.br>
2022-01-13 17:43:50 -0300

    auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import Providers from "next-auth/providers"
+
+export default NextAuth({
+
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      // @ts-ignore
+      scope: "read:user",
+    }),
+  ],
+})
