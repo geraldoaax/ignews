931fc09c467067f7d1fcd4793072ea9ff9a22821
Geraldo <geraldo@easymine.com.br>
2022-01-13 14:20:11 -0300

    api start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps, GetStaticProps } from 'next'
 import { stripe } from '../services/stripe'
 import { privateEncrypt } from 'crypto'
 
@@ -42,7 +42,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KHFRgERrPAX5ucLDF7DNRIq', {
     expand: ['product']
   })
@@ -59,7 +59,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 
 }
