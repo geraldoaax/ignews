2a39faebc69493fc5dfa2606eef5e3943cf680b1
Geraldo <geraldo@easymine.com.br>
2022-01-14 16:50:25 -0300

    create collections + faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { signIn } from "next-auth/react"
+import { fauna } from '../../../services/fauna'
+import { query as q } from "faunadb"
 
 export default NextAuth({
 
@@ -11,4 +14,18 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+  callbacks: { 
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user
+
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          {data: { email }}
+        )
+      )
+
+      return true
+    },
+  }
 })
