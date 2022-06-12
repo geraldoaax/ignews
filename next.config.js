4a67daf922ae2c5461fd99c4dbe4c1dcaae43706
Geraldo Dutra Neto <geraldoaax@gmail.com>
2022-06-12 15:18:51 -0300

    config next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ["images.prismic.io"],
-    formats: ["image/png", "image/jpg", , "image/jpeg"],
-  },
+  // images: {
+  //   domains: ["images.prismic.io"],
+  //   formats: ["image/png", "image/jpg", , "image/jpeg"],
+  // },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
